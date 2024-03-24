c1ebec7517ebc4f438668579babd9537b2f735e0
mym0404 <mym0404@gmail.com>
2024-03-24 23:44:00 +0900

    fix: fix ci

--- tool/release.sh
+++ tool/release.sh
@@ -1,7 +1,8 @@
-if cd flutter; then git pull && cd .. ; else git clone https://github.com/flutter/flutter.git; fi
+if cd flutter; then git pull && cd .. ; else git clone https://github.com/flutter/flutter.git -b stable; fi
 ls
 flutter/bin/flutter doctor
 flutter/bin/flutter clean
 flutter/bin/flutter config --enable-web
 flutter/bin/flutter pub get
+echo "" > .env
 flutter/bin/dart run build_runner build -d
