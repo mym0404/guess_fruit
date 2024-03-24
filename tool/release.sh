9efd64d2508e7ced41b27ed5a7c572765680545b
mym0404 <mym0404@gmail.com>
2024-03-24 23:37:00 +0900

    ci: fix ci

--- tool/release.sh
+++ tool/release.sh
@@ -4,4 +4,4 @@ flutter/bin/flutter doctor
 flutter/bin/flutter clean
 flutter/bin/flutter config --enable-web
 flutter/bin/flutter pub get
-flutter/bin/dart run build_runner build
+flutter/bin/dart run build_runner build -d
